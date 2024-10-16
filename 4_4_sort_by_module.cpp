40c763ecbd2e4977292772519c5e8b377d7c7670
Alexander Markov <markov_av_77@mail.ru>
2024-10-16 20:37:42 +0400

    TAsk 4_4 Final commit

--- 4_4_sort_by_module.cpp
+++ 4_4_sort_by_module.cpp
@@ -15,21 +15,15 @@ int main()
     cout << "\033[2J\033[1;1H";
     cout << "This programme ranges array by mudules of numbers\n";
     vector<int> vec = {3, 18, -100, -50, -5, 1, 10, 15, 4};
-    // cout << abs(vec[0]);
-    // int min_index = 0;
     for (int i = 0; i < vec.size(); ++i) {
         int min_index = i;
         for (int j = i + 1; j < vec.size(); ++j)
         {
-            if (vec[min_index] > vec [j]) {
+            if (abs (vec[min_index]) > abs (vec [j])) {
                 min_index = j;
             }
         }
         swap(vec[i], vec[min_index]);
-            // int temp = vec[i];
-            // vec[i] = vec[min_index];
-            // vec[min_index] = temp;
-        print_array(vec);
     }
-    // print_array(vec);
+    print_array(vec);
 }
